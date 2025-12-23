7b9e2fd6c6121ba8fd945f8d067beea6d7546d75
Youbogwang <bobogwang@naver.com>
2025-12-23 16:21:37 +0900

    feat(answer): add Answer skeleton and temporary comment for User/Question

--- backend/src/main/java/com/example/qnaboard/domain/answer/Answer.java
+++ backend/src/main/java/com/example/qnaboard/domain/answer/Answer.java
@@ -19,14 +19,14 @@ public class Answer {
     private String content;
 
     // Answer N : 1 Question
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "question_id", nullable = false)
-    private Question question;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "question_id", nullable = false)
+//    private Question question;
 
     // Answer N : 1 User
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private User user;
 
     private int vote;
 
