b834d2b2ff6e30dc1818843a5ab3d9891b931d77
simmina <s1mm1n6h@gmail.com>
2025-12-23 16:20:11 +0900

    [be] be: comment의 entity, service, responseDTO, repository 스켈레톤 코드 작성

---
+++ backend/src/main/java/com/example/qnaboard/dto/response/CommentResponse.java
@@ -0,0 +1,15 @@
+package com.example.qnaboard.dto.response;
+
+
+import java.time.LocalDateTime;
+
+public class CommentResponse {
+    private Long commentId;
+    private String content;
+    private Long userId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public CommentResponse() {
+    }
+}
---
+++ backend/src/main/java/com/example/qnaboard/service/CommentService.java
@@ -0,0 +1,30 @@
+package com.example.qnaboard.comment.service;
+
+import com.example.qnaboard.dto.response.CommentResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+
+    /* 댓글 작성 */
+    public CommentResponse createComment() {
+        return null;
+    }
+
+    /* 댓글 목록 조회 */
+    public List<CommentResponse> getComments() {
+        return null;
+    }
+
+    /* 댓글 수정 */
+    public CommentResponse updateComment() {
+        return null;
+    }
+
+    /* 댓글 삭제 */
+    public void deleteComment() {
+    }
+
+}
