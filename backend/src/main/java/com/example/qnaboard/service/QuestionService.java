76bdde5a8818613cd65500c01c9cb9ac790caa38
seoyoung <seoyoung.project@gmail.com>
2025-12-24 11:28:32 +0900

    서비스 수정

--- backend/src/main/java/com/example/qnaboard/service/QuestionService.java
+++ backend/src/main/java/com/example/qnaboard/service/QuestionService.java
@@ -1,6 +1,9 @@
 package com.example.qnaboard.service;
 
 import com.example.qnaboard.dto.QuestionResponseDTO;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -8,8 +11,8 @@ import java.util.List;
 @RequiredArgsConstructor
 @Transactional
 public class QuestionService {
-    private final QuestionRepository questionRepository;
+    // private final QuestionRepository questionRepository;
 
     // 질문 목록 조회
-    public List<QuestionResponseDTO>
+    public List<QuestionResponseDTO>questionResponseDTOS;
 }
