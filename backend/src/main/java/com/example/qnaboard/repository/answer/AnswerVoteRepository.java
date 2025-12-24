15617d125f711efc5d26d051bb247f2cfef24421
Youbogwang <bobogwang@naver.com>
2025-12-24 16:06:03 +0900

    feat: Answer controller 추가 및 AnswerErrorCode 작성

--- backend/src/main/java/com/example/qnaboard/repository/answer/AnswerVoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.qnaboard.repository.answer;
-
-import com.example.qnaboard.domain.answer.AnswerVote;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerVoteRepository extends JpaRepository<AnswerVote, Long> {
-
-    boolean existsByAnswer_IdAndUser_Id(Long answerId, Long userId);
-}
