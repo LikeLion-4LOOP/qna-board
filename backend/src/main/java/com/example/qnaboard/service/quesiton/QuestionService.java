d1d1b2628746ced35473257052d80ddb23c0aa4c
seoyoung <seoyoung.project@gmail.com>
2025-12-24 16:46:25 +0900

    dddd

--- backend/src/main/java/com/example/qnaboard/service/quesiton/QuestionService.java
+++ backend/src/main/java/com/example/qnaboard/service/quesiton/QuestionService.java
@@ -1,17 +1,17 @@
-package com.example.qnaboard.service.quesiton;
-
-// import com.example.qnaboard.dto.QuestionResponseDto;
-// import com.example.qnaboard.repository.QuestionRepository;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-@RequiredArgsConstructor
-public class QuestionService {
-    // private final QuestionRepository questionRepository;
-    // 질문 목록 조회
-    // public List<QuestionResponseDto>questionResponseDTOS;
-}
+//package com.example.qnaboard.service.quesiton;
+//
+//// import com.example.qnaboard.dto.QuestionResponseDto;
+//// import com.example.qnaboard.repository.QuestionRepository;
+//import jakarta.transaction.Transactional;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class QuestionService {
+//    // private final QuestionRepository questionRepository;
+//    // 질문 목록 조회
+//    // public List<QuestionResponseDto>questionResponseDTOS;
+//}
