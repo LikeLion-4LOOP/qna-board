9cf51e6b8a7c1e7f862d66957e6fd9707a21e2dd
yushinKim <dbtls0218@naver.com>
2025-12-28 22:28:34 +0900

    [be] userController에서 userId를 CustomUserDetails 객체에서 받아오게 변경

--- backend/src/main/java/com/example/qnaboard/controller/user/UserController.java
+++ backend/src/main/java/com/example/qnaboard/controller/user/UserController.java
@@ -2,11 +2,15 @@ package com.example.qnaboard.controller.user;
 
 import com.example.qnaboard.dto.user.request.UpdateUsernameRequest;
 import com.example.qnaboard.dto.user.response.*;
+import com.example.qnaboard.security.CustomUserDetails;
 import com.example.qnaboard.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,47 +21,45 @@ public class UserController {
 
     /**
      * 내 정보 조회
-     * @param userId
-     * @return
      */
     @GetMapping("/")
-    public UserResponse getUser(@RequestParam Long userId) {
-        return userService.getUser(userId);
+    public UserResponse getUser(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return userService.getUser(customUserDetails.getUserId());
     }
 
     // 내 정보 수정 (username)
     @PatchMapping("/")
     public UserResponse updateUsername(
-            @RequestParam Long userId,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @RequestBody UpdateUsernameRequest request
     ) {
-        return userService.updateUsername(userId, request.getUsername());
+        return userService.updateUsername(customUserDetails.getUserId(), request.getUsername());
     }
 
     // 내가 작성한 질문 목록
     @GetMapping("/questions")
     public Page<MyQuestionSummaryResponse> myQuestions(
-            @RequestParam Long userId,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
             Pageable pageable
     ) {
-        return userService.getMyQuestions(userId, pageable);
+        return userService.getMyQuestions(customUserDetails.getUserId(), pageable);
     }
 
     // 내가 작성한 답변 목록
     @GetMapping("/answers")
     public Page<MyAnswerSummaryResponse> myAnswers(
-            @RequestParam Long userId,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
             Pageable pageable
     ) {
-        return userService.getMyAnswers(userId, pageable);
+        return userService.getMyAnswers(customUserDetails.getUserId(), pageable);
     }
 
     // 내가 작성한 댓글 목록
     @GetMapping("/comments")
     public Page<MyCommentSummaryResponse> myComments(
-            @RequestParam Long userId,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
             Pageable pageable
     ) {
-        return userService.getMyComments(userId, pageable);
+        return userService.getMyComments(customUserDetails.getUserId(), pageable);
     }
 }
