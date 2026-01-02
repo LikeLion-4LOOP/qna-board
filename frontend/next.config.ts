52d40b144f4246428c0ba3587f2970deda55a3f8
yushinKim <dbtls0218@naver.com>
2026-01-02 13:05:36 +0900

    [chore] chore : frontend 배포 설정 수정 및 readme에배포 및 접속 방법 추가

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  async rewrites() {
+    const apiBaseUrl =
+      (process.env.NEXT_PUBLIC_API_BASE_URL as string) ||
+      "http://localhost:8080";
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${apiBaseUrl}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
